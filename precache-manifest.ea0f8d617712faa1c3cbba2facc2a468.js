b70ae7e126fb8b6186c886620c1b34f3b99f1aa7
jerp86 <jerp4@hotmail.com>
2020-07-15 19:13:10 -0300

    Updates

---
+++ precache-manifest.ea0f8d617712faa1c3cbba2facc2a468.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726dfe266f8bcc5c3c8f709c59350c85",
+    "url": "./index.html"
+  },
+  {
+    "revision": "746a8cab96fc30468a8f",
+    "url": "./static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "9c1fc8a742c941d0b326",
+    "url": "./static/css/main.c4c0f228.chunk.css"
+  },
+  {
+    "revision": "746a8cab96fc30468a8f",
+    "url": "./static/js/2.b1678dba.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "./static/js/2.b1678dba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1fc8a742c941d0b326",
+    "url": "./static/js/main.d2a4a135.chunk.js"
+  },
+  {
+    "revision": "5d3b09e8b24e31af45c0",
+    "url": "./static/js/runtime-main.a85e3f2d.js"
+  },
+  {
+    "revision": "224e9ff17613c2d8941f798dc882e6af",
+    "url": "./static/media/alert.224e9ff1.png"
+  },
+  {
+    "revision": "aedc9699578e2c5716aa85dc4f2ba692",
+    "url": "./static/media/background.aedc9699.png"
+  },
+  {
+    "revision": "2b37911e50fd549b2e34d9cc9e5c12da",
+    "url": "./static/media/logo.2b37911e.png"
+  },
+  {
+    "revision": "2705424923bfefa13746868b8f96d9fa",
+    "url": "./static/media/map.27054249.svg"
+  },
+  {
+    "revision": "ceee86345c47b3b940f401ba1a13598e",
+    "url": "./static/media/quarantine.ceee8634.png"
+  },
+  {
+    "revision": "558e8021e29929dabf8b038a44a1e8ca",
+    "url": "./static/media/social-distancing.558e8021.png"
+  },
+  {
+    "revision": "4aa672644567684205f9347869b0186d",
+    "url": "./static/media/virus.4aa67264.png"
+  },
+  {
+    "revision": "c7a518913cd9a26db70992047505cf51",
+    "url": "./static/media/washing-hands.c7a51891.png"
+  }
+]);
